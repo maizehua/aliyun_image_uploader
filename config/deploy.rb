03f469a378a314c8d46d39495e68d91fe97bd82e
maizehua <mai.zehua@qq.com>
2018-05-19 19:04:13 -0400

    config secret env vars

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :repo_url, "git@github.com:maizehua/aliyun_image_uploader.git"
 set :puma_threads,    [4, 16]
 set :puma_workers,    1
 
+set :user,            'deploy'
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
@@ -17,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/zehua_pair.pem) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -40,7 +41,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
+set :linked_files, fetch(:linked_files, []).push('config/local_env.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
